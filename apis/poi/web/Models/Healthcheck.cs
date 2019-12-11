2202b40943b7660addc802bad502cf799ef7db17
OpenhackerCPH <58728683+OpenhackerCPH@users.noreply.github.com>
2019-12-11 12:35:02 +0100

    Update Healthcheck.cs (#6)

--- apis/poi/web/Models/Healthcheck.cs
+++ apis/poi/web/Models/Healthcheck.cs
@@ -1,5 +1,6 @@
-﻿using System;
+using System;
 using Newtonsoft.Json;
+using System.Reflection;
 
 namespace poi.Models
 {
@@ -9,11 +10,15 @@ namespace poi.Models
         {
             Message = "POI Service Healthcheck";
             Status = "Healthy";
+            Version = Assembly.GetEntryAssembly().GetName().Version.ToString();
         }
         [Newtonsoft.Json.JsonProperty(PropertyName = "message")]
         public string Message {get;set;}
 
         [Newtonsoft.Json.JsonProperty(PropertyName = "status")]
         public string Status { get; set; }
+        
+        [Newtonsoft.Json.JsonProperty(PropertyName = "version")]
+        public string Version { get; set; }
     }
 }
